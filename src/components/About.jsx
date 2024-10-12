638373a7b941296c17420cf82d7c610977a89b93
ItsMwitah <jobmakuli042@gmail.com>
2024-10-12 20:10:21 +0300

    Remove Tilt In Mobile Devices

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
@@ -7,60 +7,110 @@ import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-    >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
-
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
-      </div>
-    </motion.div>
-  </Tilt>
+const ServiceCard = ({ index, title, icon, isMobile }) => (
+    <>
+        {isMobile ? (
+            // Render without Tilt on mobile
+            <motion.div
+                variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+                className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+            >
+                <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+                    <img
+                        src={icon}
+                        alt='web-development'
+                        className='w-16 h-16 object-contain'
+                    />
+                    <h3 className='text-white text-[20px] font-bold text-center'>
+                        {title}
+                    </h3>
+                </div>
+            </motion.div>
+        ) : (
+            // Render with Tilt on desktop
+            <Tilt className='xs:w-[250px] w-full'>
+                <motion.div
+                    variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+                    className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+                >
+                    <div
+                        options={{
+                            max: 45,
+                            scale: 1,
+                            speed: 450,
+                        }}
+                        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+                    >
+                        <img
+                            src={icon}
+                            alt='web-development'
+                            className='w-16 h-16 object-contain'
+                        />
+                        <h3 className='text-white text-[20px] font-bold text-center'>
+                            {title}
+                        </h3>
+                    </div>
+                </motion.div>
+            </Tilt>
+        )}
+    </>
 );
 
 const About = () => {
-  return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
+    const [isMobile, setIsMobile] = useState(false);
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-      >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
-      </motion.p>
+    useEffect(() => {
+        // Add a listener for screen width
+        const mediaQuery = window.matchMedia("(max-width: 768px)");
 
-      <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
-      </div>
-    </>
-  );
+        // Set initial value
+        setIsMobile(mediaQuery.matches);
+
+        // Update state if the screen size changes
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        // Attach listener
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+        // Clean up on component unmount
+        return () => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        };
+    }, []);
+
+    return (
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={styles.sectionSubText}>Introduction</p>
+                <h2 className={styles.sectionHeadText}>Overview.</h2>
+            </motion.div>
+
+            <motion.p
+                variants={fadeIn("", "", 0.1, 1)}
+                className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+            >
+                I'm a skilled software developer with experience in TypeScript and
+                JavaScript, and expertise in frameworks like React, Node.js, and
+                Three.js. I'm a quick learner and collaborate closely with clients to
+                create efficient, scalable, and user-friendly solutions that solve
+                real-world problems. Let's work together to bring your ideas to life!
+            </motion.p>
+
+            <div className='mt-20 flex flex-wrap gap-10'>
+                {services.map((service, index) => (
+                    <ServiceCard
+                        key={service.title}
+                        index={index}
+                        title={service.title}
+                        icon={service.icon}
+                        isMobile={isMobile} // Pass isMobile prop
+                    />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default SectionWrapper(About, "about");
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
@@ -9,92 +9,158 @@ import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({
-  index,
-  name,
-  description,
-  tags,
-  image,
-  source_code_link,
-}) => {
-  return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
-      >
-        <div className='relative w-full h-[230px]'>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
+                         index,
+                         name,
+                         description,
+                         tags,
+                         image,
+                         source_code_link,
+                         isMobile,
+                     }) => {
+    return (
+        <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+            {isMobile ? (
+                // Render without Tilt on mobile
+                <div className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'>
+                    <div className='relative w-full h-[230px]'>
+                        <img
+                            src={image}
+                            alt='project_image'
+                            className='w-full h-full object-cover rounded-2xl'
+                        />
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className='violet-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-            >
-              <img
-                src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
-              />
-            </div>
-          </div>
-        </div>
+                        <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+                            <div
+                                onClick={() => window.open(source_code_link, "_blank")}
+                                className='violet-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+                            >
+                                <img
+                                    src={github}
+                                    alt='source code'
+                                    className='w-1/2 h-1/2 object-contain'
+                                />
+                            </div>
+                        </div>
+                    </div>
 
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-        </div>
+                    <div className='mt-5'>
+                        <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+                        <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+                    </div>
 
-        <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-      </Tilt>
-    </motion.div>
-  );
+                    <div className='mt-4 flex flex-wrap gap-2'>
+                        {tags.map((tag) => (
+                            <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+                                #{tag.name}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+            ) : (
+                // Render with Tilt on desktop
+                <Tilt
+                    options={{
+                        max: 45,
+                        scale: 1,
+                        speed: 450,
+                    }}
+                    className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+                >
+                    <div className='relative w-full h-[230px]'>
+                        <img
+                            src={image}
+                            alt='project_image'
+                            className='w-full h-full object-cover rounded-2xl'
+                        />
+
+                        <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+                            <div
+                                onClick={() => window.open(source_code_link, "_blank")}
+                                className='violet-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+                            >
+                                <img
+                                    src={github}
+                                    alt='source code'
+                                    className='w-1/2 h-1/2 object-contain'
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='mt-5'>
+                        <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+                        <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+                    </div>
+
+                    <div className='mt-4 flex flex-wrap gap-2'>
+                        {tags.map((tag) => (
+                            <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+                                #{tag.name}
+                            </p>
+                        ))}
+                    </div>
+                </Tilt>
+            )}
+        </motion.div>
+    );
 };
 
 const Works = () => {
-  return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
+    const [isMobile, setIsMobile] = useState(false);
 
-      <div className='w-full flex'>
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
-        >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
-        </motion.p>
-      </div>
+    useEffect(() => {
+        // Add a listener for screen width
+        const mediaQuery = window.matchMedia("(max-width: 768px)");
 
-      <div className='mt-20 flex flex-wrap gap-7'>
-        {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
-        ))}
-      </div>
-    </>
-  );
+        // Set initial value
+        setIsMobile(mediaQuery.matches);
+
+        // Update state if the screen size changes
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        // Attach listener
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+        // Clean up on component unmount
+        return () => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        };
+    }, []);
+
+    return (
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={`${styles.sectionSubText} `}>My work</p>
+                <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+            </motion.div>
+
+            <div className='w-full flex'>
+                <motion.p
+                    variants={fadeIn("", "", 0.1, 1)}
+                    className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+                >
+                    Following projects showcases my skills and experience through
+                    real-world examples of my work. Each project is briefly described with
+                    links to code repositories and live demos in it. It reflects my
+                    ability to solve complex problems, work with different technologies,
+                    and manage projects effectively.
+                </motion.p>
+            </div>
+
+            <div className='mt-20 flex flex-wrap gap-7'>
+                {projects.map((project, index) => (
+                    <ProjectCard
+                        key={`project-${index}`}
+                        index={index}
+                        {...project}
+                        isMobile={isMobile} // Pass isMobile prop
+                    />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default SectionWrapper(Works, "work");
