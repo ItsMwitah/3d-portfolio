405585d1c8bfc7543d5ef9cf88b00ce4758f47e6
ItsMwitah <jobmakuli042@gmail.com>
2024-10-12 20:53:23 +0300

    Remove To Behance Logo

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -37,7 +37,7 @@ const ProjectCard = ({
                         <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
                             <div
                                 onClick={() => window.open(source_code_link, "_blank")}
-                                className='violet-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+                                className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
                             >
                                 <img
                                     src={github}
@@ -81,7 +81,7 @@ const ProjectCard = ({
                         <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
                             <div
                                 onClick={() => window.open(source_code_link, "_blank")}
-                                className='violet-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+                                className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
                             >
                                 <img
                                     src={github}
