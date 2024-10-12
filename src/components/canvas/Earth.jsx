b0764a9b18395ad9ae9131660ac31f17cf6ed954
ItsMwitah <jobmakuli042@gmail.com>
2024-10-12 19:56:02 +0300

    Remove Touch controls on phone

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,44 +1,69 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+    const earth = useGLTF("./planet/scene.gltf");
 
-  return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
-  );
+    return (
+        <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    );
 };
 
 const EarthCanvas = () => {
-  return (
-    <Canvas
-      shadows
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
-      }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          autoRotate
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Earth />
+    const [isMobile, setIsMobile] = useState(false);
 
-        <Preload all />
-      </Suspense>
-    </Canvas>
-  );
+    useEffect(() => {
+        // Add a listener for changes to the screen size
+        const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+        // Set the initial value of the `isMobile` state variable
+        setIsMobile(mediaQuery.matches);
+
+        // Define a callback function to handle changes to the media query
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        // Add the callback function as a listener for changes to the media query
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+        // Remove the listener when the component is unmounted
+        return () => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        };
+    }, []);
+
+    return (
+        <Canvas
+            shadows
+            frameloop='demand'
+            dpr={[1, 2]}
+            gl={{ preserveDrawingBuffer: true }}
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [-4, 3, 6],
+            }}
+        >
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls
+                    autoRotate
+                    enableZoom={!isMobile}      // Disable zoom on mobile
+                    enableRotate={!isMobile}    // Disable rotation on mobile
+                    enablePan={!isMobile}       // Disable panning on mobile
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                />
+                <Earth />
+
+                <Preload all />
+            </Suspense>
+        </Canvas>
+    );
 };
 
 export default EarthCanvas;
